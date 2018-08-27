836668855c49ac6f87ad12e580eca8eb485f27a6
Ismail El-Habbash <ismail.elhabbash@travelportdigital.com>
2018-08-27 22:02:57 +0100

    Initial Commit

---
+++ BinanceUI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BinanceUI
+//
+//  Created by El-habbash, Ismail on 27/08/2018.
+//  Copyright © 2018 El-habbash, Ismail. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
